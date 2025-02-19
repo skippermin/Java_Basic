a5c4a6b0a1d6e594b501ceb8fa767701dfdf4bb6
skippermin <skipper44@naver.com>
2025-02-19 23:57:17 +0900

    test 2

--- First/src/hello/HelloWorld.java
+++ First/src/hello/HelloWorld.java
@@ -3,7 +3,14 @@ package hello;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method
+		// src -> 소스 밑에 패키지가 존재한다. 
+		// bin -> 소스가 컴파일되면 클래스가 만들어지는데 bin 밑에 first가 있으며 class 파일이 위치해 있음 -> 실제로 실행되는 클래스
+		// 자바 컴파일러를 거쳐서 실행되어 진행된다. 
+		// 자바의 가상머신
+		// 자바소스코드 (Example.java)
+		// 자바 컴파일러 -> 바이트 코트(Example.class)
+		// 윈도우 가상머신 -> 윈도우에서 실행 , 리눅스 자바 가상 머신 -> 리눅스에서 실행
 		System.out.println("Hello, JAVA");
 	}
 
